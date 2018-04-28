a99c786e115d8e008b67e3261f9ce89dc35714fa
Brenden Reeves <reevesb375@gmail.com>
2018-04-28 09:55:22 -0500

    Stuff

--- commands/league/suminfo.js
+++ commands/league/suminfo.js
@@ -1,19 +1,13 @@
 const dotenv = require('dotenv').config();
-var http = require('http');
 const request = require('request');
-
-//League API
-var lol = require('node-of-legends');
-lol.setConfig({
-    region: lol.REGION.NORTH_AMERICA,
-    apikey: 'RGAPI-ab5befbc-caf7-4675-b526-437f942d3ade'
-});
 
 //MUST HAVE
 exports.run = (client, msg, args) => {
-    if(args[2] == null)
+    console.log('I MADE IT')
+    console.log(args +`\nhttps://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${args[2]}?api_key=${process.env.LEAGUE}`)
+    if(args[2] == '')
         args[2] = "I AFK 4 Butts"
-    //Grab User ID
+    //Grab User ID 
     request(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${args[2]}?api_key=${process.env.LEAGUE}`, { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
         var sumPrint = ("__**Summoner Info**__\n"+
