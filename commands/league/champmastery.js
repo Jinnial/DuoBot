e28c915eb37f2904c331067a6c01760d60933682
Unknown <enoragon@gmail.com>
2018-04-28 11:14:45 -0500

    Cut the Logs

--- commands/league/champmastery.js
+++ commands/league/champmastery.js
@@ -10,10 +10,8 @@ exports.run = (client, msg, args) => {
     //Grab User ID
     request(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${args[2]}?api_key=${process.env.LEAGUE}`, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
-        console.log(body);
         request(`https://na1.api.riotgames.com/lol/champion-mastery/v3/scores/by-summoner/${body.id}?api_key=${process.env.LEAGUE}`, { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
-            console.log(body);
             msg.channel.send("``` Total Champion Mastery Score: " + body + "```");
         });
     });
